ac7def866589f93868135cfe1bd2196b52b11638
Lucas Carrozzino <lucas.carrozzino@ibm.com>
2023-06-17 19:26:02 -0300

    normalização string

--- tabelaVerdade - Copia.hs
+++ tabelaVerdade - Copia.hs
@@ -2,3 +2,23 @@ import Data.List
 import System.IO
 import Control.Monad
 
+
+normExp :: [Char] -> [[Char]]                              -- Normaliza a String
+normExp exp
+    | null exp = []
+    | head exp == '(' = normExp (tail exp)
+    | head exp == ')' = normExp (tail exp)
+    | head exp == ';' = ";" : normExp (tail exp)
+    | head exp == '?' = "?" : normExp (tail exp)
+    | head exp == '*' = "*" : normExp (tail exp)
+    | head exp == '>' = ">" : normExp (tail exp)
+    | head exp == '<' = "<" : normExp (tail exp)
+    | head exp == '[' = "[" : normExp (tail exp)
+    | head exp == ']' = "]" : normExp (tail exp)
+    | otherwise = (head exp :[]) : normExp (tail exp)
+
+
+main = do
+    entrada <- getLine
+    let norm = normExp entrada
+    print norm
