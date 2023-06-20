6bdf12ab6b7820531b81d75ca00f69ccbc4e4675
Guilherme Moreira <gfmoreira@id.uff.br>
2023-06-20 19:02:09 -0300

    Create Programa.hs

---
+++ Programa.hs
@@ -0,0 +1,22 @@
+module Estrutura.Grafo (
+  Grafo(..),
+  destinosVertice,
+  programasVertice,
+  verticeTemPrograma,
+  Programa,
+  Vertice
+) where
+
+type Destino = Int
+type Programa = String
+type Vertice = [(Destino, Programa)]
+type Grafo = [Vertice]
+
+destinosVertice :: Vertice -> [Destino]
+destinosVertice vertice = map (\ponto -> fst ponto) vertice
+
+programasVertice :: Vertice -> [Programa]
+programasVertice vertice = map (\ponto -> snd ponto) vertice
+
+verticeTemPrograma :: Vertice -> Programa -> Bool
+verticeTemPrograma vertice arvore = arvore `elem` (programasVertice vertice)
