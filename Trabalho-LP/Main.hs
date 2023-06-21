53c7ab18b2281c6886d100d9671bef80a13e7e7e
Lucas Carrozzino <lucas.carrozzino@ibm.com>
2023-06-20 22:22:32 -0300

    aa

--- Trabalho-LP/Main.hs
+++ Trabalho-LP/Main.hs
@@ -4,9 +4,8 @@ import Estrutura.Grafo
 import System.IO
 import VerificadordePdls
 
-main :: IO()
 main = do
   --entrada
-  putStrLn $ verificaFramePDL [[(1, "alpha")], [(2, "gama")], []] (criaArvore "Arvore (Arvore Vazia \"alpha\" Vazia) \";\" (Arvore Vazia \"beta\" Vazia)")
+  putStrLn $ verificaFramePDL [[(1, "alpha")], [(2, "gama")], [(3, "gama")]] (criaArvore "Arvore (Arvore Vazia \"alpha\" Vazia) \";\" (Arvore Vazia \"beta\" Vazia)")
 
 
