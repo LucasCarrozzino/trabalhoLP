68a93aca3deb5c9c54182a5c7d1b9ce19f7bee26
Lucas Carrozzino <lucas.carrozzino@ibm.com>
2023-11-16 22:02:15 -0300

    test

--- trabalhoLP.hs
+++ trabalhoLP.hs
@@ -1,3 +1,20 @@
+import Data.List
+
+-- Função principal
+main :: IO ()
+main = do
+    putStrLn "Informe a grade no formato 'V|M|0;M|M|0;V|M|V;' (exemplo: 'M|V|M;V|0|M;0|V|0;'): "
+    input <- getLine
+    case createMatrixFromString input of
+        Just matrix -> do
+            putStrLn "Grade inicial:"
+            printMatrix matrix
+            putStrLn "Informe o número máximo de iterações: "
+            limit <- getLine
+            dustToDust 0 (read limit) matrix 0
+        Nothing -> do
+            putStrLn "Por favor, informar uma grade no formato suportado."
+            main
 
 -- Função para dividir uma string em elementos separados por um caractere
 splitByChar :: Char -> String -> [String]
@@ -31,42 +48,60 @@ createMatrix (row:rest) = splitByChar '|' row : createMatrix rest
 printMatrix :: [[String]] -> IO ()
 printMatrix matrix = mapM_ (putStrLn . unwords) matrix
 
--- Função para simular as interações e imprimir o tabuleiro no estado final e 
---a quantidade de passos necessários para ele ser estabilizado
-dustToDust :: Int -> Int ->  [[String]] -> Int -> IO ()--Erro: tEstavel sempre = 0
+-- Função principal para iterações
+dustToDust :: Int -> Int ->  [[String]] -> Int -> IO ()
 dustToDust tAtual tMax mundo tEstavel
-  | tAtual <= tMax = if seeAll mundo [[""]] 0 0 == mundo then dustToDust (tAtual+1) tMax (seeAll mundo [[""]] 0 0) tEstavel else dustToDust (tAtual+1) tMax (seeAll mundo [[""]] 0 0) tAtual
+  | tAtual < tMax = do
+        let novoMundo = seeAll mundo [[""]] 0 0
+        putStrLn $ "Iteração " ++ show (tAtual + 1) ++ ":"
+        printMatrix novoMundo
+        if novoMundo == mundo
+            then do
+                putStrLn $ "O sistema se estabilizou na iteração " ++ show (tAtual + 1) ++ "."
+                putStrLn $ "Quantidade total de iterações: " ++ show (tAtual + 1 + tEstavel)
+            else
+                dustToDust (tAtual + 1) tMax novoMundo (tEstavel + 1)
   | otherwise = do
-     putStrLn "A versão final do tabuleiro:"
-     printMatrix mundo
-     putStrLn "A quantidade de interações necessárias para o tabuleiro se estabilizar foi "
-     print (tEstavel + 1)
+        putStrLn "A versão final do tabuleiro:"
+        printMatrix mundo
+        putStrLn $ "A quantidade de interações necessárias para o tabuleiro se estabilizar foi " ++ show (tEstavel + 1)
 
---Varre o tabuleiro e contrói a próxima interação do mesmo
-seeAll :: [[String]] -> [[String]] -> Int -> Int ->  [[String]]--Erro:resultado sempre igual m;m
+-- Função para obter o valor de uma célula em determinadas coordenadas
+getValue :: [[String]] -> Int -> Int -> String
+getValue mundo x y =
+    if x >= 0 && y >= 0 && x < length mundo && y < length (head mundo)
+        then mundo !! x !! y
+        else ""
+
+-- Função para contar o número de células vizinhas com um valor específico
+countNeighbors :: [[String]] -> Int -> Int -> String -> Int
+countNeighbors mundo x y value =
+    let neighbors = filter (\(i, j) -> getValue mundo i j == value) (adjacentCells x y)
+    in length neighbors
+
+-- Função para obter as coordenadas das células adjacentes
+adjacentCells :: Int -> Int -> [(Int, Int)]
+adjacentCells x y =
+    [(i, j) | i <- [x - 1 .. x + 1], j <- [y - 1 .. y + 1], i /= x || j /= y]
+
+-- Função para verificar as regras e obter o próximo estado de uma célula
+updateCell :: [[String]] -> Int -> Int -> [[String]]
+updateCell mundo x y
+    | currentValue == "0" && liveNeighbors == 3 = [["V"]]
+    | currentValue == "V" && zombieNeighbors > 0 = [["Z"]]
+    | currentValue == "V" && (liveNeighbors < 2 || liveNeighbors > 3) = [["0"]]
+    | currentValue == "M" && liveNeighbors == 3 = [["V"]]
+    | currentValue == "Z" && liveNeighbors == 0 = [["0"]]
+    | otherwise = [[currentValue]]
+  where
+    currentValue = getValue mundo x y
+    liveNeighbors = countNeighbors mundo x y "V"
+    zombieNeighbors = countNeighbors mundo x y "Z"
+
+-- Função para criar a próxima iteração do tabuleiro
+seeAll :: [[String]] -> [[String]] -> Int -> Int ->  [[String]]
 seeAll mundo futuro x y
   | length mundo == length futuro && length (head mundo) == length (head futuro) =  futuro
-  | otherwise = if x < length mundo then seeAll  mundo (futuro <> catBox mundo x y) (x + 1) y else  seeAll  mundo (futuro <> catBox mundo x y) 0 (y + 1)
-
---Vê qual deve ser o próximo estado de uma célula
-catBox :: [[String]] -> Int -> Int -> [[String]]
-catBox mundo x y = [["m"]]--falta checar quais os estados vizinhos e compara-los para saber qual letra retornar
-
-main :: IO ()
-main = do
-    putStrLn "Informe o tabuleiro"
-    input <- getLine
-    case createMatrixFromString input of
-        Just matrix -> do
-            putStrLn "carregando tabuleiro..."
-            printMatrix matrix
-            putStrLn "Informe o número máximo de interações"
-            limit <- getLine
-            dustToDust 0 (read limit) matrix 0
-        Nothing -> do
-            putStrLn "Por favor, informar um tabuleiro no formato suportado"
-            main
-
--- função ver(matriz, aux)
--- aux.tamanho ==matriz.tamanho?return aux
--- (proxima coord de aux)  = matrix[x==2?x:0][x==2?y:y+1]
+  | otherwise = if x < length mundo
+                then seeAll mundo (futuro <> updateCell mundo x y) (x + 1) y
+                else  seeAll mundo (futuro <> updateCell mundo x y) 0 (y + 1)
